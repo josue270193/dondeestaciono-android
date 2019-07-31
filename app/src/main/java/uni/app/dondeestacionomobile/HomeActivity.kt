e510ff4955a95f00e97f35b68e4698958e1c839a
Josue D <josue270193@gmail.com>
2019-07-31 01:08:27 -0300

    Commit Inicial

---
+++ app/src/main/java/uni/app/dondeestacionomobile/HomeActivity.kt
@@ -0,0 +1,50 @@
+package uni.app.dondeestacionomobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import uni.app.dondeestacionomobile.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityHomeBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_home)
+
+        drawerLayout = binding.drawerLayout
+
+        navController = findNavController(R.id.home_nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // Set up ActionBar
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Set up navigation menu
+//        binding.navigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/uni/app/dondeestacionomobile/fragment/MapsFragment.kt
@@ -0,0 +1,40 @@
+package uni.app.dondeestacionomobile.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import uni.app.dondeestacionomobile.R
+import uni.app.dondeestacionomobile.databinding.FragmentMapsBinding
+
+class MapsFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMapsBinding.inflate(inflater, container, false)
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val caba = LatLng(-34.6165417,-58.386224)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(caba))
+    }
+}
